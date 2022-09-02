1e7e79814dd8ae9f111020ddab30b968ae4a3f5f
Simon Dupkala <simon.dupkala@gmail.com>
2022-09-02 22:05:28 +0100

    fixed footer

--- src/components/Lokalita.js
+++ src/components/Lokalita.js
@@ -5,30 +5,50 @@ export default function Lokalita(){
   return (
     <div style={{"backgroundColor":"#6ba5b4"}}>
         <div className='container lokalita'>
+            
             <div className='d-flex justify-content-center'>
             <img id="gpsImg" src={gps} style={{"padding":"50px"}}/>
             </div>
-            <h2>najit lokalitu</h2>
-            <p>Stavte se a řekněte "ahoj" v některé z našich poboček.</p>
-        </div>
+            
+            <div>
+            <h1 id='najitLokalitu'>najit lokalitu.</h1>
+            <h4 className='pb-4'>Stavte se a řekněte "ahoj" v některé z našich poboček.</h4>
+        
         <form className='container'>
-  <div class="form-group row">
-    <div class="col-sm-10">
-      <input type="email" class="form-control" id="inputEmail3" placeholder="Email"/>
-    </div>
-  </div>
-  <div class="form-group row py-3">
-    <div class="col-sm-10">
-      <input type="password" class="form-control" id="inputPassword3" placeholder="Password"/>
-    </div>
-  </div>
- 
-  <div class="form-group row">
-    <div class="col-sm-10" style={{"width":"100%"}}>
-      <button type="submit" class="btn btn-primary">Sign in</button>
-    </div>
-  </div>
+            <div class="form-group row">
+                <div >
+                <input
+                type="email"
+                class="form-control"
+                id="inputEmail3"
+                style={{"height":"60px"}}
+                placeholder="PSČ / město"/>
+                </div>
+            </div>
+            <div class="form-group row py-3">
+                <div>
+                <input
+                type="password"
+                class="form-control" 
+                id="inputPassword3" 
+                style={{"height":"60px"}} 
+                placeholder="obor"/>
+                </div>
+            </div>
+            <div class="form-group row">
+                <div style={{"width":"100%"}}>
+                <button
+                    type="submit" 
+                    id="btnHledej" 
+                    class="btn btn-primary">
+                    hledat
+                    </button>
+                </div>
+            </div>
 </form>
+
+</div>
+</div>
     </div>
   )
 }
